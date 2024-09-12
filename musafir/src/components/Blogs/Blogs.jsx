7a39844b443a861ee2210a44a65c9aee55d84ab9
Parth Maha <parthmaha9@gmail.com>
2024-09-12 23:58:30 +0530

    added after cie completion

---
+++ musafir/src/components/Blogs/Blogs.jsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import vr from '../../assets/VR.jpg'
+import chauta from '../../assets/Chauta.jpg'
+
+
+
+function Blogs() {
+  return (
+    <div>
+
+  <div class="mx-auto max-w-7xl px-2">
+    <div class="flex flex-col space-y-8 pb-10 pt-12 md:pt-24">
+      <p class="text-3xl font-bold text-gray-900 md:text-5xl md:leading-10">
+        Resources and insights
+      </p>
+      <p class="max-w-4xl font-semibold text-gray-600 md:text-left">
+      Looking to explore and get ideas about a new place? Use our platform to search for key insights on top attractions, and hidden gems. Whether you're interested in historical landmarks, outdoor adventures, or dining hotspots, our detailed info and user reviews will help you discover the best experiences. Plan your trips, and make your visit unforgettable.
+      </p>
+
+      <div class="mt-6 flex w-full items-center space-x-2 md:w-1/3">
+        <input
+          class="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+          type="text"
+          placeholder="Search Places"
+        />
+        <button
+          type="button"
+          class="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+        >
+          Search
+        </button>
+      </div>
+    </div>
+    <div class="mt-10 hidden w-full flex-col justify-between space-y-4 md:flex md:flex-row">
+      <div class="flex w-full items-end border-b border-gray-300">
+        <div class="cursor-pointer px-4 py-2 text-base font-semibold leading-normal text-gray-700 first:border-b-2 first:border-black">
+          Trending
+        </div>
+        <div class="cursor-pointer px-4 py-2 text-base font-semibold leading-normal text-gray-700 first:border-b-2 first:border-black">
+          Cafe
+        </div>
+        <div class="cursor-pointer px-4 py-2 text-base font-semibold leading-normal text-gray-700 first:border-b-2 first:border-black">
+          Malls
+        </div>
+        <div class="cursor-pointer px-4 py-2 text-base font-semibold leading-normal text-gray-700 first:border-b-2 first:border-black">
+          Historical Places
+        </div>
+      </div>
+    </div>
+    <div class="grid gap-6 gap-y-10 py-6 md:grid-cols-2 lg:grid-cols-3">
+      <div class="border">
+        <img
+          src={vr}
+          class="aspect-video w-auto rounded-md"
+          alt=""
+        />
+        <div class="min-h-min p-3">
+          <p class="mt-4 w-full text-xs font-semibold leading-tight text-gray-700">
+            #Malls
+          </p>
+          <p class="mt-4 flex-1 text-base font-semibold text-gray-900">
+            VR Mall
+          </p>
+          <p class="mt-4 w-full text-sm leading-normal text-gray-600">
+          VR Surat offers a curated mix of local and global experiences across retail, food and entertainment.
+            Connecting Communities.
+            Shop. Dine. Explore.    
+          </p>
+        </div>
+      </div>
+      <div class="border">
+        <img
+          src="https://surattourism.in/images/places-to-visit/header/dumas-beach-surat-tourism-entry-fee-timings-holidays-reviews-header.jpg"
+          class="aspect-video w-45 rounded-md"
+          alt=""
+        />
+        <div class="min-h-min p-3">
+          <p class="mt-4 w-full text-xs font-semibold leading-tight text-gray-700">
+            #Beach
+          </p>
+          <p class="mt-4 flex-1 text-base font-semibold text-gray-900">
+            Dumas
+          </p>
+          <p class="mt-4 w-full text-sm leading-normal text-gray-600">
+          Surat's most famous and crowded beach is a great place to relax with family.The beach offers many adventure sports and activities such as camel riding and baby sports at affordable costs. You can even bring your own toys to enjoy. Don't forget to visit the Dariya Ganesh temple
+          </p>
+        </div>
+      </div>
+      <div class="border">
+        <img
+          src={chauta}
+          class="aspect-video w-full rounded-md"
+          alt=""
+        />
+        <div class="min-h-min p-3">
+          <p class="mt-4 w-full text-xs font-semibold leading-tight text-gray-700">
+            #Shopping
+          </p>
+          <p class="mt-4 flex-1 text-base font-semibold text-gray-900">
+            Chauta
+          </p>
+          <p class="mt-4 w-full text-sm leading-normal text-gray-600">
+          Chauta Bazaar is one of the oldest market of Surat.Chauta Bazaar's history dates back to early 1700s. The present-day Mota Mandir in Chauta was known as Vaishnao Haveli. There were shops surrounding the Haveli where the women visiting the temple would go shopping for utensils, clothes, cosmetics, groceries and other day-to-day household goods.
+          </p>
+        </div>
+      </div> 
+    </div>
+    
+  </div>
+  <div class="mx-auto mt-12 max-w-7xl bg-gray-50">
+
+  </div>
+</div>
+
+
+  )
+}
+
+export default Blogs
---
+++ musafir/src/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Blogs from './components/Blogs/Blogs.jsx'
+import Login from './components/Login/Login.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='blogs' element={<Blogs />} />
+      <Route path='login' element={<Login />} />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
