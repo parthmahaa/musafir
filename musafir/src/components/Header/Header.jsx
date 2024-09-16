1edc267e141a4a8f722a4fea40f6db6d4be18798
Parth Maha <parthmaha9@gmail.com>
2024-09-16 19:12:28 +0530

    changed blogs to musafir

--- musafir/src/components/Header/Header.jsx
+++ musafir/src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="/login"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-gray-800 hover:bg-gray-200 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
                         </Link>
@@ -67,7 +67,7 @@ export default function Header() {
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
-                                    Blogs
+                                    Explore
                                 </NavLink>
                             </li>
                             
