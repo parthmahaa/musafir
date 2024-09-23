16a427e7cd031d26a01db44f93ce2bc4d163da44
Parth Maha <parthmaha9@gmail.com>
2024-09-23 19:13:51 +0530

    added photo to home section

--- musafir/src/components/Home/Home.jsx
+++ musafir/src/components/Home/Home.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import home from '../../assets/home.jpg'
 
 export default function Home() {
         return(
             <>
-            <div class="relative w-full">
+            <section class="relative py-10 overflow-hidden bg-black sm:py-16 lg:py-24 xl:py-32">
+    <div class="absolute inset-0">
+        <img class="object-cover w-full h-full md:object-left md:scale-150 md:origin-top-left" src={home} alt="" />
+    </div>
+
+    <div class="absolute inset-0 hidden bg-gradient-to-r md:block from-black to-transparent"></div>
+
+    <div class="absolute inset-0 block bg-black/60 md:hidden"></div>
+
+    <div class="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+        <div class="text-center md:w-2/3 lg:w-1/2 xl:w-1/3 md:text-left">
+            <h2 class="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl">Discover top destinations</h2>
+            <p class="mt-4 text-base text-gray-200">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam.</p>
+
+            <form action="#" method="POST" class="mt-8 lg:mt-12">
+                <div class="flex flex-col items-center sm:flex-row sm:justify-center">
+                    <div class="flex-1 w-full min-w-0 px-4 sm:px-0">
+                        <div class="relative text-gray-400 focus-within:text-gray-600">
+                            <label for="email" class="sr-only"></label>
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                explore 
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+            {/* <div class="relative w-full">
   <div class="relative isolate z-0 bg-white px-6 pt-14 lg:px-8">
     <div class="relative mx-auto max-w-2xl py-24">
       <div class="absolute inset-x-0 -top-[4rem] -z-10 transform-gpu overflow-hidden blur-3xl md:-top-[10rem]">
@@ -42,7 +74,7 @@ export default function Home() {
       </div>
     </div>
   </div>
-</div>
+</div> */}
 
             </>
         )
