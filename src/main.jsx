dbd7d4977a576a3dee144d5894760e05d77f7b39
Parth Maha <parthmaha9@gmail.com>
2024-10-06 10:25:00 +0530

    changed route configuration method

--- src/main.jsx
+++ src/main.jsx
@@ -2,49 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Route, RouterProvider, BrowserRouter as Router, Routes,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Explore from './components/Explore/Explore.jsx'
 import LoginPage from './LoginPage.jsx'
 import SignupPage from './SignupPage.jsx'
-
-
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Layout/>,
-      children: [
-        {
-          path: '',
-          element: <Home/>
-        },
-        {
-          path: 'about',
-          element: <About/>
-        },
-        {
-          path: 'explore',
-          element: <Explore/>
-        }
-      ]
-    },
-    {
-      path: '/login',
-      element: <LoginPage/>
-    },
-    {
-      path: '/signup',
-      element: <SignupPage/>
-    }
-    
-  ]
-)
+import Cafe from './components/Categories/Cafe/Cafe.jsx'
+import Malls from './components/Categories/Malls/Malls.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="explore" element={<Explore />} />
+          <Route path="cafe" element={<Cafe />} />
+          <Route path="malls" element={<Malls />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
