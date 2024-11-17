58272d14889ae72430fa301b92deabc5b8b9f0b8
Parth Maha <parthmaha9@gmail.com>
2024-11-17 17:11:15 +0530

    updated

--- src/components/Categories/StreeFood/StreetFood.jsx
+++ src/components/Categories/StreeFood/StreetFood.jsx
@@ -193,7 +193,7 @@ function StreetFood() {
           ))
         ) : (
           <div className="col-span-full flex justify-center items-center py-12">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-orange-400"></div>
           </div>
         )}
       </div>
