258b8e1db6f3347f8e1451625492ccb41a65395a
Parth Maha <parthmaha9@gmail.com>
2024-11-23 22:30:54 +0530

    updated

--- src/components/ContactUs/Contact.jsx
+++ src/components/ContactUs/Contact.jsx
@@ -7,45 +7,60 @@ function Contact() {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const form = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+
 
   let history = useNavigate();
 
   const sendMail = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
 
-    const response = await fetch('https://musafir-4lbu.onrender.com/contact-us', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name: name, email: email }),
-    });
-
-    const json = await response.json();
-
-    if (json.success) {
-      emailjs
-        .sendForm('service_23y8sdp', 'template_1mmca0s', form.current, {
-          publicKey: 'ElFYig7j9EWHUIp2x',
-        })
-        .then(
-          () => {
-            console.log('Mail sent!');
-            toast.success('You have been subscribed');
-            history('/');
-          },
-          (error) => {
-            toast.error('Enter a valid mail');
-            console.log('FAILED...', error.text);
-          }
-        );
-      console.log('Data sent');
-    } else {
-      history('/contact-us');
-      toast.error('email already exists');
+    try{
+      const response = await fetch('https://musafir-4lbu.onrender.com/contact-us', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: name, email: email }),
+      });
+  
+      const json = await response.json();
+  
+      if (json.success) {
+        emailjs
+          .sendForm('service_23y8sdp', 'template_1mmca0s', form.current, {
+            publicKey: 'ElFYig7j9EWHUIp2x',
+          })
+          .then(
+            () => {
+              console.log('Mail sent!');
+              toast.success('You have been subscribed',{
+                position: "top-center",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+              });
+              history('/');
+            },
+            (error) => {
+              toast.error('Enter a valid mail');
+              console.log('FAILED...', error.text);
+            }
+          );
+        console.log('Data sent');
+      } else {
+        history('/contact-us');
+        toast.error('email already exists');
+      }
+    }catch(e){
+      toast.error("Failed to subscribe, try again.")
+    }finally{
+      setIsLoading(false);
     }
   };
-
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.key === 'Enter') {
@@ -59,11 +74,6 @@ function Contact() {
   }, []);
   return (
     <div className="flex flex-col pt-20 items-center justify-center text-center min-h-100">
-      {/* <div className="h-15 w-50 mb-6 pb-5 md:mb-0">
-                        <Link to="/" className="flex items-center">
-                        <img src={img1} id="logo" className="max-w-23 filter-none max-h-6" alt="" />
-                        </Link>
-                    </div> */}
       <div className="flex flex-col justify-center items-center bg-[#ffffff] p-8 border-2 border-[#264143] rounded-2xl shadow-[3px_4px_0px_1px_#E99F4C] w-auto">
         <p className="text-[#264143] font-extrabold text-2xl mb-5">
           Newsletter
@@ -105,7 +115,15 @@ function Contact() {
               type="submit"
               className="py-3 px-4 w-72 text-white bg-black rounded-lg font-bold text-sm shadow-[3px_3px_0px_0px_#E99F4C] hover:opacity-80 focus:translate-y-1"
             >
-              SUBSCRIBE
+              {isLoading ? (
+                    <div className="flex items-center justify-center">
+                      <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
+                    </div>
+                  ) : (
+                    <div className="flex font-bold justify-center items-center">
+                      Subscribe
+                    </div>
+                  )}
             </button>
             <p className="text-gray-400 font-semibold mt-4 text-center w-full max-w-[18rem]">
               By subscribing to our newsletter, you agree to receive newsletters
