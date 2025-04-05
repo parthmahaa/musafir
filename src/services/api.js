6394d1a099864d12f54ebbfee6cbe89f6d6d1bad
Parth Maha <parthmaha9@gmail.com>
2025-04-06 00:29:59 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_DOMAIN || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_DOMAIN,
   headers: {
     'Content-Type': 'application/json',
   }
