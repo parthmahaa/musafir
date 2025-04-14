7a116b5613494e126f38b6f425d8b78b00700a7f
Parth Maha <parthmaha9@gmail.com>
2025-04-14 12:49:54 +0530

    updated origin access

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const port = process.env.PORT || 5000
 
 import cors from 'cors'
 app.use(cors({
-    origin: '*',
+    origin: ['https://musafir-main.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
