c61839971a4b5e572e3f1d7a3fd098e7d906a146
Parth Maha <parthmaha9@gmail.com>
2025-05-13 18:50:55 +0530

    add logs

--- backend/routes/cafe.js
+++ backend/routes/cafe.js
@@ -6,15 +6,23 @@ const router = Router()
 router.get('' , [] ,  async (req,res) => {
     try{
         const cacheKey = 'cafe'
+
+         console.time('redis-get');
         const cachedData = await client.get(cacheKey)
+        console.timeEnd('redis-get')
+        
         if(cachedData){
+            console.log("cache hit");
             return res.status(200).json({msg: JSON.parse(cachedData) ,cached: true})
         }
+
+        console.log("no cahed data");
 
         const response = await Cafe.find().sort({ rating: -1 })
         if(!response) res.status(404).json({msg : "No data found"})
 
         await client.setEx(cacheKey, 86400, JSON.stringify(response))
+        console.log("Data cached");
         res.status(200).json({msg: response ,cached: false})
     }
     catch(err){
